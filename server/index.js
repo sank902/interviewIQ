468813be4aee4c8de5ce5a7abaf3dc9dd6a67f17
Sankalp <kharesankalp57@gmail.com>
2026-05-26 12:16:50 +0530

    Update CORS for Vercel domain

--- server/index.js
+++ server/index.js
@@ -11,9 +11,9 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+    origin: ["http://localhost:5173", "https://interview-iq-swart.vercel.app"],
+    credentials: true
+}));
 
 app.use(express.json())
 app.use(cookieParser())
